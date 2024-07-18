3e508365c0c5d98b30a0b2bee67159ff29a316ef
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-07-18 23:19:08 +0530

    Add container and Button

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,35 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  fun() {
+    print("button clicked");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: Text("Billionair App!"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.blueGrey[600],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Balance"),
+              ElevatedButton(
+                onPressed: fun,
+                child: Text("Click Here"),
+              ),
+            ],
+          ),
         ),
       ),
     );
